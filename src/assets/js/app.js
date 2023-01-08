dfd5f4c01bbfec29540e78b737457a8ff0615837
unknown <solalex.s.a@gmail.com>
2023-01-08 12:57:11 +0300

    add form

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", () => {
       direction: 'horizontal',
       speed: 1000,
       spaceBetween: 100,
-      loop: true,
       allowTouchMove: false,
     });
 
@@ -39,6 +38,58 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     new SliderController('.main-swiper-btns', swiper);
+  }
+
+  if (document.querySelector('.main-form-select')) {
+    class Select {
+      constructor(wrapper) {
+        this.select = document.querySelector(wrapper);
+        this.btn = this.select.querySelector('.main-form-select__btn');
+        this.list = this.select.querySelector('.main-form-select-list');
+        this.value = this.select.querySelector('.main-form-select__value');
+        this.isOpen = false;
+        this.init();
+      }
+
+      init() {
+        this.getListHeight();
+        this.btn.onclick = (e) => this.toggleList.call(this, e);
+        [...this.list.children].forEach(item => {
+          const btn = item.querySelector('.main-form-select-list-item__btn');
+          btn.onclick = this.setValue.bind(this, btn);
+        })
+        document.onclick = this.closeList.bind(this);
+      }
+
+      getListHeight() {
+        this.listHeight = [...this.list.children].reduce((acc, item) => {
+          return acc += item.offsetHeight;
+        }, 0) / 10;
+      }
+
+      toggleList(e) {
+        e.stopPropagation();
+        this.isOpen ? this.closeList() : this.openList();
+      }
+
+      closeList() {
+        this.list.style.maxHeight = 0;
+        this.isOpen = false;
+      }
+
+      openList() {
+        this.list.style.maxHeight = `${this.listHeight}rem`;
+        this.isOpen = true;
+      }
+
+      setValue(btn) {
+        this.btn.innerText = btn.innerText;
+        this.btn.classList.remove('_placeholder')
+        this.value.value = btn.dataset.value;
+      }
+    }
+
+    new Select('.main-form-select');
   }
 
 })
