835ac40eca49b1fd9d7854bb08279478ff12ee38
Aleksandr <solalex.s.a@gmail.com>
2023-01-26 14:13:46 +0300

    fix svg

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -41,6 +41,35 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  // SMOOTH SCROLL
+  SmoothScroll({
+    // Время скролла 400 = 0.4 секунды
+    animationTime: 800,
+    // Размер шага в пикселях 
+    stepSize: 60,
+
+    // Дополнительные настройки:
+
+    // Ускорение 
+    accelerationDelta: 30,
+    // Максимальное ускорение
+    accelerationMax: 2,
+
+    // Поддержка клавиатуры
+    keyboardSupport: true,
+    // Шаг скролла стрелками на клавиатуре в пикселях
+    arrowScroll: 50,
+
+    // Pulse (less tweakable)
+    // ratio of "tail" to "acceleration"
+    pulseAlgorithm: true,
+    pulseScale: 4,
+    pulseNormalize: 1,
+
+    // Поддержка тачпада
+    touchpadSupport: true,
+  })
+  //<==
 
   // LOADER 
   if (LOADER) {
@@ -207,7 +236,7 @@ document.addEventListener("DOMContentLoaded", () => {
         trigger: triggerContainer,
         start: "top top",
         end: "bottom bottom",
-        scrub: true,
+        scrub: 2,
       }
     });
     tl.from(text, {
